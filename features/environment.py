66a9badb984f7cc5b48d6fba23d054af9cba9768
Guilherme Augusto Sousa Silva <guilherme.silva@normatel.com.br>
2026-02-27 13:51:07 -0300

    :sparkles: feat: Regras de etapas

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,8 @@
+import os
+import allure
 from core.driver_factory import create_driver
+from allure_commons.types import AttachmentType
+from datetime import datetime
 
 
 def before_scenario(context, scenario):
@@ -6,4 +10,23 @@ def before_scenario(context, scenario):
 
 
 def after_scenario(context, scenario):
-    context.driver.quit()
+    if scenario.status == "failed" and hasattr(context, "driver"):
+        os.makedirs("reports/screenshots", exist_ok=True)
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        screenshot_name = f"reports/screenshots/{scenario.name}_{timestamp}.png"
+        context.driver.save_screenshot(screenshot_name)
+
+    if hasattr(context, "driver"):
+        context.driver.quit()
+
+
+def after_step(context, step):
+    if step.status == "failed":
+        os.makedirs("reports/screenshots", exist_ok=True)
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        screenshot_path = f"reports/screenshots/{step.name}_{timestamp}.png"
+        context.driver.save_screenshot(screenshot_path)
+
+        allure.attach.file(
+            screenshot_path, name="Screenshot", attachment_type=AttachmentType.PNG
+        )
